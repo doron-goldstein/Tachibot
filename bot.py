ebad8207f9e1998a0111da0f4110db6bc9fdceef
SynderBlack <frogermanz@gmail.com>
2019-02-19 00:50:11 +0200

    dont fail if theres no description

--- bot.py
+++ bot.py
@@ -19,7 +19,8 @@ class TachiBoti(discord.Client):
 
         title = manga.title.get('english') or manga.title.get('romaji') or manga.title.get('native')
         desc = "***" + ", ".join(manga.genres) + "***\n"
-        desc += manga.description[:256 - len(desc) - 7] + f"[...]({manga.site_url})"
+        if manga.description is not None:
+            desc += manga.description[:256 - len(desc) - 7] + f"[...]({manga.site_url})"
         e = discord.Embed(title=title, description=desc, color=0x4286f4)
         e.set_thumbnail(url=manga.cover_image)
         e.url = manga.site_url
