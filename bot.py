7625ed121543cfd44137b5fe18b66b406da76321
Justin Kyle Ramos <de-odex@users.noreply.github.com>
2020-01-08 01:17:37 +0800

    Implement anime search and embed

--- bot.py
+++ bot.py
@@ -8,24 +8,30 @@ import kadal
 class TachiBoti(discord.Client):
     def __init__(self):
         super().__init__()
-        self.regex = re.compile(r"<((?!https?:\/\/.*)(?!a?:.*:).*?)>")
+        self.manga_regex = re.compile(
+            r"(?!`)<((?!https?:\/\/.*)(?!a?:.*:).*?)>(?!`)")
+        self.anime_regex = re.compile(r"{((?!https?:\/\/.*)(?!a?:.*:).*?)}")
         self.tachi_id = 349436576037732353
         self.klient = kadal.Klient(loop=self.loop)
 
-    async def format_embed(self, name):
+    async def format_embed(self, name, anime=False):
         try:
-            manga = await self.klient.search_manga(name, popularity=True)
+            if anime:
+                media = await self.klient.search_anime(name, popularity=True)
+            else:
+                media = await self.klient.search_manga(name, popularity=True)
         except kadal.MediaNotFound:
             return
 
-        title = manga.title.get('english') or manga.title.get('romaji') or manga.title.get('native')
-        desc = "***" + ", ".join(manga.genres) + "***\n"
-        if manga.description is not None:
-            desc += manga.description[:256 - len(desc)] + f"... [(more)]({manga.site_url})"
-        desc = desc.replace("<br>", "").replace("<i>", "").replace("</i>", "")  # dirty half-fix until i figure something better out
+        title = media.title.get('english') or media.title.get('romaji') or media.title.get('native')
+        desc = "***" + ", ".join(media.genres) + "***\n"
+        if media.description is not None:
+            desc += media.description[:256 - len(desc)] + f"... [(more)]({media.site_url})"
+        # dirty half-fix until i figure something better out
+        desc = desc.replace("<br>", "").replace("<i>", "").replace("</i>", "")
         e = discord.Embed(title=title, description=desc, color=0x4286f4)
-        e.set_thumbnail(url=manga.cover_image)
-        e.url = manga.site_url
+        e.set_thumbnail(url=media.cover_image)
+        e.url = media.site_url
         return e
 
     async def on_ready(self):
@@ -51,7 +57,7 @@ and the channel is updated regularly to reflect the status of extensions and the
     async def on_message(self, message):
         if message.author == self.user:  # Ignore own messages
             return
-        m = self.regex.findall(message.clean_content)
+        m = self.manga_regex.findall(message.clean_content)
         if m:
             if len(m) > 1:
                 fmt = ""
@@ -64,6 +70,23 @@ and the channel is updated regularly to reflect the status of extensions and the
                 await message.channel.send(fmt)
             else:
                 embed = await self.format_embed(m[0])
+                if not embed:
+                    return
+                await message.channel.send(embed=embed)
+
+        a = self.anime_regex.findall(message.clean_content)
+        if a:
+            if len(a) > 1:
+                fmt = ""
+                for name in a:
+                    try:
+                        anime = await self.klient.search_anime(name, popularity=True)
+                        fmt += "<" + anime.site_url + ">\n"
+                    except kadal.MediaNotFound:
+                        pass
+                await message.channel.send(fmt)
+            else:
+                embed = await self.format_embed(a[0], anime=True)
                 if not embed:
                     return
                 await message.channel.send(embed=embed)
