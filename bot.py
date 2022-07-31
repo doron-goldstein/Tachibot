f1d0e6b86599cc93c21400425b2fd81dfaf628be
Soitora <simon.mattila@protonmail.com>
2022-07-31 20:43:38 +0200

    Fixes (#11)
    * Fix HTML encoded characters
    
    * Fix series type style
    
    Multi-words like "Anime_short" is now "Anime Short"
    
    Abbrevations like "Ona" is now "ONA"

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import re
+import html
 import textwrap
 
 import discord
@@ -44,9 +45,11 @@ class TachiBoti(discord.Client):
             (r"</?br/?>", "\n")
         ]
         for regex, regex_replace in replacements:
-            desc = re.sub(regex, regex_replace, desc, flags=re.I | re.M)
+            desc = html.unescape(re.sub(regex, regex_replace, desc, flags=re.I | re.M))
         footer = re.sub(r".*\.", "", str(media.format))
-        footer_text = footer.replace("TV", "ANIME").capitalize()
+        footer_text = footer.replace("TV", "ANIME").replace("_", " ").title()
+        if len(footer_text) <= 3:
+            footer_text = footer_text.upper()
         status = re.sub(r".*\.", "", str(media.status))
         status_text = status.replace("_", " ").capitalize()
 
